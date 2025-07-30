c451116602f377c8b243a5a8d8669d5d4b15308c
Safayet <hossainsafayet187@gmail.com>
2025-07-31 00:52:13 +0600

    Feat:initial database design complete

---
+++ database/migrations/2025_07_30_164844_create_leads_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('leads', function (Blueprint $table) {
+            $table->id();
+            $table->date("lead_date");
+            $table->string("email");
+            $table->string("name");
+            $table->string("phone");
+            $table->string("interested_course");
+            $table->string("interested_country");
+            $table->string("current_qualification");
+            $table->string("ielts_or_english_test")->nullable();
+            $table->string("soruce")->nullable();
+            $table->foreignId("status_id")->nullable()->constrained('lead_statues')->cascadeOnUpdate()->restrictOnDelete();
+            $table->text('notes')->nullable();
+            $table->foreignId('assigned_branch')->nullable()->constrained('branches')->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('assigned_user')->nullable()->constrained('users')->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('created_by')->constrained('users')->cascadeOnUpdate()->restrictOnDelete();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('leads');
+    }
+};
