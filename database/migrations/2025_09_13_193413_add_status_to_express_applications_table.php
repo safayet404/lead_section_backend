c21d37fc898a1bee8942ae9859a1bf8394e66dff
Safayet <hossainsafayet187@gmail.com>
2025-09-14 01:36:10 +0600

    Add status field in express applicationn field

---
+++ database/migrations/2025_09_13_193413_add_status_to_express_applications_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('express_applications', function (Blueprint $table) {
+            $table->enum('status',['pending','assigned','converted'])->default('pending');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('express_applications', function (Blueprint $table) {
+            //
+        });
+    }
+};
